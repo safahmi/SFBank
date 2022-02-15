9bfd3d87d74a8abc9e37b652e8e4fe1dee4a456c
safahmi <97179134+safahmi@users.noreply.github.com>
2022-02-15 16:21:19 +0100

    clean up client class

--- src/components/Client.java
+++ src/components/Client.java
@@ -1,62 +1,57 @@
 package components;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 //1.1.1 Creation of the client class
 
 public class Client {
-	
-//	public static void main(String[] args) {
-//			Client client1 = new Client("Fahmi", "Samir");
-//			Client client2 = new Client("Loulou", "poulou");
-//			System.out.println(client1);
-//			System.out.println(client2);
-//				
-//			
-//		}
-	
-		
-	//Attributes 
+
+	// Attributes
 	private String name;
 	private String firstName;
 	private int clientNumber;
 	private static final AtomicInteger count = new AtomicInteger(0);
-	
-	//Constructor 
-	public Client(String value1, String value2) {
-		this.name = value1;
-		this.firstName = value2;
+
+	// Constructor
+	public Client(String name, String firstName) {
+		this.name = name;
+		this.firstName = firstName;
 		this.clientNumber = count.incrementAndGet();
 	}
-	
 
-	//Accessors end Mutators of each attribute
-	private String getName() {
+	// Accessors end Mutators of each attribute
+	public String getName() {
 		return name;
 	}
-	private void setName(String name) {
+
+	public void setName(String name) {
 		this.name = name;
 	}
-	private String getFirstName() {
+
+	public String getFirstName() {
 		return firstName;
 	}
-	private void setFirstName(String firstName) {
+
+	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	private int getClientNumber() {
+
+	public int getClientNumber() {
 		return clientNumber;
 	}
-	private void setClientNumber(int clientNumber) {
+
+	/**
+	 * client number is automatically handle and this method seems to be dangerous
+	 * 
+	 * @param clientNumber
+	 */
+	public void setClientNumber(int clientNumber) {
 		this.clientNumber = clientNumber;
 	}
-	
-	// toString() method
+
 	@Override
-	public String toString() {	  
-		return "(" + this.name + ", " + this.firstName + ")";
-	}  
-	
+	public String toString() {
+		return "(" + this.name + ", " + this.firstName + ", " + this.clientNumber + ")";
+	}
 
 }
